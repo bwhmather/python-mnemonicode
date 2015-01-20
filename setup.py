ed97a40d8b06d632dd3a309ca4d07daee714aef0
Ben Mather <bwhmather@bwhmather.com>
2015-01-20 15:26:40 +0000

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='mnemonicode',
     url='https://github.com/bwhmather/mnemonicode',
-    version='0.1.0',
+    version='0.1.1',
     author='Ben Mather',
     author_email='bwhmather@bwhmather.com',
     maintainer='',
